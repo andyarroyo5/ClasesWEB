ccbf095d12efd6e127ec635e22cf7be690d97b9d
Andrea Arroyo <sonia.arroyo@udem.edu>
2017-03-01 05:44:53 -0600

    WEB corregidos
    P1 y P2

--- CarlosOrtega-344956P1.php
+++ CarlosOrtega-344956P1.php
@@ -1,93 +1,163 @@
 <HTML>
-<meta charset="UTF-8">
-<HEAD>
-   <TITLE>Subir Archivo</TITLE>
-</HEAD>
-<BODY>
+   <meta http-equiv = "Content-Type" content = "text/html; charset=utf-8" />
+   <HEAD>
+      <TITLE>Práctica 1 WEB</TITLE>
+   </HEAD>
+   <BODY>
+      <h2> Práctica 1 </h2>
+      <form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>">
 
-<?php
-	if($_SERVER["REQUEST_METHOD"] == "POST")
-   {
-		echo "<h2>Datos del archivo cargado</h2>";
+         <div style="float:left;">
+           Introduce una palabra a buscar <input type="text" name="palabra" style="margin-bottom:30px;" required><br>
+           <span>Selecciona método de búsqueda: </span>
+           <input type="radio" name="tipo" value="web" required> Página web
+           <input type="radio" name="tipo" value="archivo"> Archivo<br><br>
+
+          Selecciona un archivo (Max 2 MB):
+          <input type="hidden" name="MAX_FILE_SIZE" VALUE="2000000">
+          <!-- accept: filtra archivos con esas características -->
+          <input type="file" name="archivo" accept="text/plain, .rtf" >ó
+          &nbsp; Página Web <input placeholder="URL" type="text" name="sitio" style="margin-bottom:30px;">  <br>
+          <input type="submit" value="Buscar" style="margin-top:20px;">
+           <br>
+          </div>
+
+       </form>
+       <pre style="float:left;">
+          <hr />
+
+          <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST")
+            {
+
+                //checamos si dio click en archivo o en sitio web
+                switch ($_POST['tipo']) {
+
+                   //Seleccionó de archivo
+                   case 'archivo':
+                     echo '<br><h2>Búsqueda por Archivo</h2>';
+                     //print_r($_FILES['archivo']);
+                     @$archivo=$_FILES['archivo'];
+
+                     //Validar que el archivo se cargo correctamente
+                     if(is_uploaded_file($archivo["tmp_name"]))
+                     {
+                        //Validar que haya cargado un archivo de texto
+                        if($_FILES['archivo']['type']!='text/plain' && $_FILES['archivo']['type']!='application/rtf')
+                        {
+                           echo 'Archivo no válido';
+                           return;
+                        }
+
+                        $nombre = $archivo["name"];
+                        //Eliminar símbolos en nombre de archivo
+                        //$nombre = preg_replace("/[^a-zA-Z0-9._-]/", "", $nombre);
+                        //echo "<br>Nombre limpio: $nombre <br>";
+
+                        $contenido=file_get_contents($_FILES['archivo']['tmp_name']);
+                        //Eliminar símbolos en contenido
+                        $contenido=preg_replace("/(?![=$'€%])\p{P}/u", "", $contenido);
+
+                      }
+                      else{
+                           @$error=$archivo["error"];
+                           if($error==UPLOAD_ERR_INI_SIZE)
+                              echo "Tamaño de php.ini excedido";
+                          else if($error==UPLOAD_ERR_FORM_SIZE)
+                              echo "Tamaño de formulario excedido";
+                           else
+                              echo "Posiblemente no se cargó el archivo <BR>";
+                           return;
+                        }
+
+                      break;
+
+                  //Seleccionó Página WEB
+                   case 'web':
+
+                     $sitio = $_POST["sitio"];
+
+                     echo '<br><h2>Búsqueda por Página Web</h2>';
+
+                     //Validar que input no este vacío
+                     if(!isset($sitio) || trim($sitio) == '')
+                     {
+                        echo "No incluiste liga de una página web";
+                        return;
+                     }
+                     //Validar si el sitio web existe
+                    $existe = true;
+                    $file_headers = @get_headers($sitio);
+                    if(!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') {
+                        $existe = false;
+                        echo 'No se encontró el sitio: '. $sitio;
+                        return;
+                    }else {
+                        $existe = true;
+                    }
+
+                     $contenido=file_get_contents($sitio);
+                     echo "<br><br>Búsqueda por Sitio<br>";
+                     echo "Nombre del sitio : $sitio<br>";
+
+                     //limpia el contenido de tags html
+                     $contenido=strip_tags($contenido);
+                     //limpia el string de símbolos
+                     $contenido = preg_replace('/[^\p{L}\p{N}\s]/u', '', $contenido);
+
+                     break;
+
+                   default:
+                      echo "Error en validar la búsqueda";
+                      break;
+                }
 
 
+                //Quitar líneas o espacios extras
 
-		@$archivo=$_FILES["archivo"];
-		if(is_uploaded_file($archivo["tmp_name"]))
-		{
-		  	echo '<b>Datos en $FILES["archivo"]:</b> <br>';
-			foreach ($_FILES["archivo"] as $i => $e)
-			{
-				echo "<b>$i</b> = ";
-				var_dump($e);
-				echo "<br>";
-			}
+                $contenido=trim(preg_replace("/\s+/", " ",$contenido));
+                //echo $contenido;
+                //mayúsculas
+                $contenido=strtoupper($contenido);
+                // string a Array
+                $palabrasArray= explode(" ", $contenido);
+                //Contar valores dentro del array de palabras Repetidas, Array Asociativo
+                $palabrasRepetidas= array_count_values($palabrasArray);
+                //ordenar de mayor a menor
+                arsort($palabrasRepetidas);
+                //print_r($palabrasRepetidas);
 
-      if($_FILES["archivo"]['type']!="text/plain" && $_FILES["archivo"]['type']!="text/rtf" )
-        echo "Error archivo no es Archivo de Texto";
-        echo "<br>";
+               echo "<br /><i> Resultado de Búsqueda</i> <br>";
 
-			$nombre = $archivo["name"];
-			$nombre = preg_replace("/[^a-zA-Z0-9._-]/", "", $nombre);
-			echo "<br>Nombre limpio: $nombre <br>";
+               echo "<br>Palabra por buscar : ";
+               $palabra=$_POST['palabra'];
+               $palabra= preg_replace('/[^\p{L}\p{N}\s]/u', '', $palabra);
+               $palabra=trim($palabra);
+               echo $palabra. "<br>";
 
-      echo "<br>Palabra<br>";
-      $palabra=$_POST['palabra'];
-      echo $palabra;
+               //Buscar en el array la palabra de búsqueda
+               $resultadoBusqueda=  @$palabrasRepetidas[strtoupper($palabra)]? : 0;
+               if($resultadoBusqueda== 0){
+                 echo "<br />La palabra <b>$palabra </b> no aparece";
+               }
+               else{
+                 echo "<br />La palabra <b> ". $palabra ." </b> aparece ". ($resultadoBusqueda+1)." veces";
+               }
 
-      // Le dice al Browser que no parse en HTML
-    //  header('Content-Type: text/plain');
-    //  echo "<br>Contenido<br>";
-      $contenido=file_get_contents($_FILES['archivo']['tmp_name']);
-      //echo $contenido;
-      		echo "<br><br>Búsqueda<br>";
-  //    $patron = preg_quote($palabra, '/');
-      $patron =  "/^.*$palabra.*\$/m";
-      if(preg_match_all($patron, $contenido, $matches)){
-         //separa datos de arreglo
-         echo implode("<br>", $matches[0])."<br>";
-      //   print_r($matches);
-      echo "<br> Se encontraron ".$number." instancias de ".$palabra;
-      }
-      else{
-         echo "No se encontraron instancias de esta palabra";
-      }
+                //Imprimir los primeros 10 resultados
+                echo "<br /><br> <i>Palabras Repetidas</i> <br><br>";
+                 $i=0;
+                 foreach ($palabrasRepetidas as $palabra => $numRepeticion) {
+                    $i++;
+                    if($i==11)
+                   {
+                      break;
+                   }
+                   echo $i. '. '. $palabra. ":". ($numRepeticion+1). "<br />";
 
-      $number =count($matches[0]);
-
-
-
-	 	}
-	  	else
-	  	{
-   		@$error=$archivo["error"];
-
-
-   		if($error==UPLOAD_ERR_INI_SIZE)
-				echo "Tamaño de php.ini excedido";
-   		elseif($error==UPLOAD_ERR_FORM_SIZE)
-				echo "Tamaño de formulario excedido";
-      else
-				echo "Posiblemente no se cargó el archivo <BR>";
-	   }
-	  return;
-	}
-?>
-<h2> Subir Archivo </h2>
-<form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>">
-         Selecciona un archivo (Max 1 MB):
-         <input type="hidden" name="MAX_FILE_SIZE" VALUE="1048576">
-			<input type="file" name="archivo" required> <br>
-      <span>Introduce una palabra a buscar: </span>
-       <input placeholder="Palabra" type="text" name="palabra" required="true"></input>
-      <br>
-      <!--<span>Selecciona en donde buscar: </span>
-      <input type="radio" name="tipo" value="web"> Página web
-      <input type="radio" name="tipo" value="archivo"> Archivo<br>
-      <span>Selecciona un archivo </span><input type="file" name="file" value="1048576" required><br>
-      <span hidden="archivo" show="web">Página Web: </span> <input hidden="archivo" placeholder="url" required="true"></input>-->
-         <input type="submit" name="enviar" value="Enviar" />
-</form>
-
-</BODY>
+                 }
+             }
+         ?>
+       </pre>
+   </BODY>
 </HTML>
--- CarlosOrtega-344956P2.php
+++ CarlosOrtega-344956P2.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Práctica 2 WEB </title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
@@ -70,7 +70,7 @@
     /*foreach ($tags as $tag) {
         var_dump($tag->textContent);
     }*/
-    echo "<br/>".@$tags[0]->textContent;
+    echo "<br/> ". number_format((float)$cantidad, 2, '.', '') ." MXN son ". @$tags[0]->textContent;
 
     }
     else {
